f4c35a8bbb2fad2b5710091440f7991c8593abcb
anero <anero79@gmail.com>
2020-07-14 20:27:37 -0300

    Agrego instancias de tipo m5.large como opción

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -13,7 +13,7 @@ module Peluchero
       end
 
       def instance_type_options
-        [ 't3.micro', 't3.small', 't3.medium', 't3.large', 't3.xlarge', 't3.2xlarge' ]
+        [ 'm5.large', 't3.micro', 't3.small', 't3.medium', 't3.large', 't3.xlarge', 't3.2xlarge' ]
       end
 
       def server_image_options(server_images)
